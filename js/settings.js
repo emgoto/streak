81db68025554990e2d7a5f1e7bc93ad68952980e
Bentley <bentley.s.cook@gmail.com>
2019-09-09 14:11:51 -0500

    Add iframe auto-resizing
    Because the content of the t.popup may change based on styles in Trello
    changing, we should resize the iframe using t.sizeTo automatically so
    that it is always the correct height.

--- js/settings.js
+++ js/settings.js
@@ -3,45 +3,53 @@
 var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
 
-// Show remove button and currently stored values if habitTrackerSettings exists
-t.get('card', 'shared', 'habitTrackerSettings')
-.then(function(habitTrackerSettings) {
-  if (habitTrackerSettings) {
-    document.getElementById('remove-btn').classList.remove('u-hidden');
-        
-    document.querySelectorAll("#settings_form [name='daysInWeek']")[0].value = habitTrackerSettings.daysInWeek;
-    document.querySelectorAll("#settings_form [name='firstDayOfWeek']")[0].value = habitTrackerSettings.firstDayOfWeek;
-  }
-}).catch(function(){
-     // do nothing
-  });
+t.render(function () {
+  // make sure your rendering logic lives here, since we will
+  // recall this method when data is changed that the Power-Up might care
+  // about like data stored via t.set
+
+  // Show remove button and currently stored values if habitTrackerSettings exists
+  t.get('card', 'shared', 'habitTrackerSettings')
+    .then(function (habitTrackerSettings) {
+      if (habitTrackerSettings) {
+        document.getElementById('remove-btn').classList.remove('u-hidden');
+
+        document.querySelectorAll("#settings_form [name='daysInWeek']")[0].value = habitTrackerSettings.daysInWeek;
+        document.querySelectorAll("#settings_form [name='firstDayOfWeek']")[0].value = habitTrackerSettings.firstDayOfWeek;
+      }
+    }).catch(function () {
+      // do nothing
+    }).finally(function () {
+      t.sizeTo(document.body);
+    });
+});
 
 // Stores settings in habitTrackerSettings when user presses save button
-document.getElementById('save-btn').addEventListener('click', function(){
-  
+document.getElementById('save-btn').addEventListener('click', function (){
+
   const daysInWeek = document.querySelectorAll("#settings_form [name='daysInWeek']")[0].value;
   const firstDayOfWeek = document.querySelectorAll("#settings_form [name='firstDayOfWeek']")[0].value;
-  
+
   return t.set('card', 'shared', 'habitTrackerSettings', {daysInWeek, firstDayOfWeek})
-  .then(function(){
+  .then(function (){
     t.closePopup();
-  }).catch(function(){
+  }).catch(function (){
       // do nothing
   });
 });
 
 // Removes habitTrackerSettings when user presses remove button
-document.getElementById('remove-btn').addEventListener('click', function(){
+document.getElementById('remove-btn').addEventListener('click', function (){
   return t.set('card', 'shared', 'habitTrackerSettings', null)
-        .then(function(){
-          t.closePopup();
-        }).catch(function(){
-            // do nothing
-        });
+    .then(function (){
+      t.closePopup();
+    }).catch(function (){
+        // do nothing
+    });
 });
 
 // Brings up information modal when user clicks "How to use Streak"
-document.getElementById('how-to-use').addEventListener('click', function(){
+document.getElementById('how-to-use').addEventListener('click', function (){
   return t.modal({
     url: './modal.html',
     height: 360,
